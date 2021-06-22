220ed64cfa3e05728c2da1c056f0177b62380f45
Kevinvaldez <86176535+Kevinvaldez@users.noreply.github.com>
2021-06-22 18:07:28 -0500

    Update matrises.cpp
    El nombre completo y específico la parte que me toco

--- matrises.cpp
+++ matrises.cpp
@@ -8,7 +8,7 @@ float matriz2[10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
 float suma[10],resta[10],producto[10],division[10];
 
 
-// esta, es, la, parte, de,valdez.
+// esta, es, la, parte, de, valdez, Mendoza, Kevin, Ismael.
 	
 	
 	do {
